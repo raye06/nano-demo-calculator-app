a416d96d0fc53e06d609778b85a0617afea66113
raye06 <raye.sowmya@gmail.com>
2023-09-15 22:10:40 +0530

    added comments

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ app.use(express.json());
 
 const baseRouter = express.Router();
 
+//get request to print hello world
 baseRouter.get('/greeting', (req, res) => {
     return res.send('Hello world!');
 });
 
+//post request that performs addition
 baseRouter.post('/add', (req, res) => {
     const { first, second } = req.body;
     res.json({ "result": first + second });
 });
 
+//post request that performs subtraction
 baseRouter.post('/subtract', (req, res) => {
     const {first, second} = req.body;
     res.json({ "result": first - second });
